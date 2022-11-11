41c1c2ad238aa2e958831fc372ec59755eef8f32
ubeyde <mustafaubeydeezici@gmail.com>
2022-11-11 07:48:35 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product = new Product();
+        product.name = "laptop";
+        product.id = 1;
+        product.description = "Asus Laptop";
+        product.price = 15000;
+        product.stockAmount = 3;
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
     }
 }
